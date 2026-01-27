bc20206ec23af23d71dee27f775982d8deeff5aa
perfect-Bee <joonslee3@gmail.com>
2026-01-27 12:15:35 +0900

    fix : Lv.3. N+1 문제 : todo @Query를 @EntityGraph로 변경

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,13 +12,13 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+    // Todo 전체 조회 : 수정일 기준 내림차순 + 연관된 user를 즉시 로딩 (N+1 문제 방지)
+    @EntityGraph(attributePaths = "user")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN FETCH t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+    // Todo 조회(todoId) : 연관된 User를 즉시 로딩
+    @EntityGraph(attributePaths = "user")
+    Optional<Todo> findById(Long todoId); // @Param 삭제, 매서드명에 WithUser : 이걸 쿼리로 읽음
 
     int countById(Long todoId);
 }
